a74de191ea8fe303e0f86ee07bb3321f37fd0b85
johnmolin91 <john.molin91@gmail.com>
2018-09-16 15:18:46 -0400

    site loads

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 var connection = require("./connection.js");
 
 var orm = {
-    all: function(cb) {
+    all: function(tableInput, cb) {
         var queryString = "SELECT * FROM playersadvanced LIMIT 20;"
         connection.query(queryString, function(err, result) {
             if (err) {
