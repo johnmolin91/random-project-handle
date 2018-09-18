6497b2c18c1412ffc299902073615b679b81a115
johnmolin91 <john.molin91@gmail.com>
2018-09-18 15:47:58 -0400

    working on player show

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,2 +1,12 @@
 $(document).ready(function() {
+    $(".playerButton").on("click", function() {
+        for (var i = 0; i++; i < this.length) {
+            var x = document.getElementById("playerStats");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+    });
 });
